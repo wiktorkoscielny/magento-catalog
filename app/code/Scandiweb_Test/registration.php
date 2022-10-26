4610ae4f6a3b40df979b7b24f81cd39796085057
Wiktor Koscielny <wiktor.koscielny@scandiweb.com>
2022-10-26 07:16:45 +0200

    Fixed registartion.php file

--- app/code/Scandiweb_Test/registration.php
+++ app/code/Scandiweb_Test/registration.php
@@ -1,7 +1,9 @@
-<?php 
+<?php
+
 use Magento\Framework\Component\ComponentRegistrar;
-	\Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Scandiweb_Test',
-	__DIR__
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Scandiweb_Test',
+    __DIR__
 );
